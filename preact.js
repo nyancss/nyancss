54b6e365def185a4e41fe93667244add591adbae
Sasha Koss <koss@nocorp.me>
2017-06-21 17:42:21 +0200

    Create React & Preact wrappers

---
+++ preact.js
@@ -0,0 +1,4 @@
+var decss = require('.')
+var h = require('react').h
+
+module.exports = decss.bind(null, h)
---
+++ react.js
@@ -0,0 +1,4 @@
+var decss = require('.')
+var React = require('react')
+
+module.exports = decss.bind(null, React.createClass.bind(React))
