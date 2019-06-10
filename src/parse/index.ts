0042dbb5b291ea35e84c7affe6ffbacef8af1d39
Sasha Koss <koss@nocorp.me>
2019-06-10 09:37:05 +0400

    Extract utils to a package

--- src/parse/index.ts
+++ src/parse/index.ts
@@ -1,4 +1,9 @@
 import { NyanCSSMap } from '@nyancss/types'
+import {
+  applyComponent,
+  tryApplyBooleanProp,
+  tryApplyEnumProp
+} from '@nyancss/utils'
 import { parse as parseCSS } from 'postcss'
 import parser from 'postcss-selector-parser'
 
@@ -30,82 +35,4 @@ function extractClasses(selector: string) {
     })
   }).processSync(selector)
   return classNames
-}
-
-const booleanPropRegEx = /^(\w+)-(\w+)$/
-
-function tryApplyBooleanProp(map: NyanCSSMap, className: string) {
-  const booleanPropCaptures = className.match(booleanPropRegEx)
-  if (booleanPropCaptures) {
-    const [, componentName, propName] = booleanPropCaptures
-    applyComponent(map, componentName)
-
-    const prop = map[componentName].props[propName]
-    if (!prop) {
-      map[componentName].props[propName] = {
-        propName,
-        type: 'boolean',
-        className
-      }
-    } else if (prop && prop.type === 'enum') {
-      prop.values.push(true)
-      prop.classNames.true = className
-    }
-    return true
-  }
-  return false
-}
-
-const enumPropRegEx = /^(\w+)-(\w+)-(\w+)$/
-
-function tryApplyEnumProp(map: NyanCSSMap, className: string) {
-  const enumPropCaptures = className.match(enumPropRegEx)
-  if (enumPropCaptures) {
-    const [, componentName, propName, value] = enumPropCaptures
-    applyComponent(map, componentName)
-
-    let prop = map[componentName].props[propName]
-    if (!prop) {
-      prop = map[componentName].props[propName] = {
-        propName,
-        type: 'enum',
-        values: [],
-        classNames: {}
-      }
-    } else if (prop.type === 'boolean') {
-      prop = map[componentName].props[propName] = {
-        propName,
-        type: 'enum',
-        values: [true],
-        classNames: {
-          true: prop.className
-        }
-      }
-    }
-
-    prop.values.push(value)
-    prop.classNames[value] = className
-    return true
-  }
-  return false
-}
-
-function applyComponent(
-  map: NyanCSSMap,
-  componentName: string,
-  className?: string
-) {
-  map[componentName] =
-    map[componentName] || emptyComponent(componentName, className)
-  if (!map[componentName].className && className)
-    map[componentName].className = className
-}
-
-function emptyComponent(componentName: string, className: string | undefined) {
-  return {
-    componentName,
-    tag: undefined,
-    className,
-    props: {}
-  }
 }
